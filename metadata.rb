285318e5989196ed3cd61d12319192e80571d7f9
Sean M. Sullivan <sean@barriquesoft.com>
2014-08-01 16:40:52 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'server-base'
+maintainer       'Pulselocker, Inc.'
+maintainer_email 'sean@pulselocker.com'
+license          'Apache 2.0'
+description      'Installs/Configures server-base'
+long_description 'Installs/Configures server-base'
+version          '0.1.0'
+
