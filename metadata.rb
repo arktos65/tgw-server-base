da18d16fb6cde2ed8e9af56a4c6dadac7cc14275
Sean M. Sullivan <sean@tgwconsulting.co>
2020-12-08 08:10:08 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'sean@tgwconsulting.co'
 license          'Apache-2.0'
 description      'Installs/Configures base server environment'
 long_description 'Installs/Configures base server environment'
-version          '2.5.1'
+version          '2.6'
 issues_url       'https://github.com/arktos65/tgw-server-base-cookbook/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/arktos65/tgw-server-base-cookbook' if respond_to?(:source_url)
 chef_version     '>= 13'
